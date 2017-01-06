2d6cc07977439d9e486a55ccceb1eb8d5d339518
jessgates <gates.jess@gmail.com>
2017-01-06 10:13:17 -0600

    Initial Commit

---
+++ Brew_BuddyTests/Brew_BuddyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Brew_BuddyTests.swift
+//  Brew_BuddyTests
+//
+//  Created by Jess Gates on 1/6/17.
+//  Copyright © 2017 Jess Gates. All rights reserved.
+//
+
+import XCTest
+@testable import Brew_Buddy
+
+class Brew_BuddyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
