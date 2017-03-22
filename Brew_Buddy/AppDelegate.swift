a994c2b1b46161663da63c3e5ecbe90e1a6e8425
jessgates <gates.jess@gmail.com>
2017-03-22 12:07:19 -0500

    Add simple notificatons on entry.

--- Brew_Buddy/AppDelegate.swift
+++ Brew_Buddy/AppDelegate.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    
+    let locationManager = CLLocationManager()
     
     // Set the data model
     let dataStack = CoreDataStack(modelName: "BrewBuddyDataModel")!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        locationManager.delegate = self
         
         // Set the style of the tab bar, status bar, and navigation bar across the app
         let navigationBarAppearance = UINavigationBar.appearance()
@@ -42,6 +47,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         dataStack.save()
+    }
+    
+    func handleEvent(forRegion region: CLRegion!) {
+        alertForNearBrewery()
+    }
+    
+    func alertForNearBrewery() {
+        let alertController = UIAlertController(title: "You're by a brewery!", message: "Stop in and have a drink!", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (UIAlertAction) in
+        }
+        alertController.addAction(okAction)
+        window?.rootViewController?.present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        if region is CLCircularRegion {
+            handleEvent(forRegion: region)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        if region is CLCircularRegion {
+            handleEvent(forRegion: region)
+        }
     }
 }
 
